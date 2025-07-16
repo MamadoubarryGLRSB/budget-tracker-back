4e9972b107ed97e4f17c348947079f0e02e56c53
MamadoubarryGLRSB <mamadou01.barry@ism.edu.sn>
2025-07-16 15:24:02 +0200

    add main

--- nest-api/src/main.ts
+++ nest-api/src/main.ts
@@ -38,6 +38,7 @@ async function bootstrap() {
 
   SwaggerModule.setup('api', app, document, customOptions);
 
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
