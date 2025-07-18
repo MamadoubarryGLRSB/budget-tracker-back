cad7322fa5b8fd68bff8e7af61a0fb4110de79fc
MamadoubarryGLRSB <mamadou01.barry@ism.edu.sn>
2025-07-18 15:48:34 +0200

    Fix: Add AppController and AppService to AppModule - Fix root endpoint 404

--- nest-api/src/app.module.ts
+++ nest-api/src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { AccountModule } from './account/account.module';
@@ -24,5 +26,7 @@ import { RecipientModule } from './recipient/destinataire.module';
     StatisticsModule,
     RecipientModule,
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
